b5a957366dc1129d99018eae67df75ff03cbb142
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-05-17 21:13:32 +0200

    Make input splitting independent of parallel execution

--- parasolc.sh
+++ parasolc.sh
@@ -26,17 +26,24 @@ if has_compilation_errors < "${tmp_dir}/analysis-output.json"; then
     exit 0
 fi
 
+i=0
+contracts_in_output < "${tmp_dir}/analysis-output.json" | while IFS= read -r selected_contract_json; do
+    select_contract "$selected_contract_json" \
+        < "${tmp_dir}/input.json" \
+        > "${tmp_dir}/partial-input-${i}.json"
+    ((++i))
+done
+
 # Modify original input again, this time to get a full set of inputs, each for a single contract.
 # Use xargs to run solc on each of the new inputs in parallel.
-export -f select_contract
+# TODO: Use same numbers for input and corresponding output.
 subprocess_script="
     pid=\$\$
-    select_contract '{}' < '${tmp_dir}/input.json' \
-        | '$SOLC_BINARY' ${SOLC_ARGS[*]} \
+    '$SOLC_BINARY' ${SOLC_ARGS[*]} < '{}' \
         | jq --indent 4 \
         >> '${tmp_dir}'/partial-output-\${pid}.json
 "
-contracts_in_output < "${tmp_dir}/analysis-output.json" \
+find "${tmp_dir}" -name 'partial-input-*.json' \
     | xargs --max-procs=0 --delimiter=$'\n' -I {} bash -c "$subprocess_script"
 
 # Combine the .contracts from each output file. These should not overlap.
